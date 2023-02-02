7e328bc28c658ae8c97a0ef0b6abf9260aa47525
Tobias Videla G <tobiasvidelaug14@gmail.com>
2023-02-02 00:04:37 -0300

    agregando modales para los redireccionamientos

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,67 @@
 
 // - Redirigir
 const credits = document.querySelector('#creditos'),
-      iconos = document.querySelector('.materia-icono');
+      iconos = document.querySelectorAll('.materia-icono');
 
 credits.addEventListener('click', () => {
     swal({
-        title: 'Creditos',
+        text: '¿Deseas redirigirte al portafolio del creador de esta página?',
+        buttons: {
+            no: {
+                text: 'Permanecer aquí',
+                value: 'stay',
+            },
+            si: {
+                text: 'Ir a la página',
+                value: 'leave',
+            },
+        },
+    })
+    .then(value => {
+        switch (value) {
+            case "stay":
+                break;
+            case "leave":
+                swal({
+                    icon: 'success',
+                    title: 'En breve irás allí',
+                });
+                setTimeout(() => {
+                    window.open('https://tobiasvidela.github.io/CV-Portfolio-TUVG/','_self');
+                }, 2000);
+                break;
+        };
     });
+});
+
+iconos.forEach(i => {
+    swal({
+        text: `Estás por ir a ${i.name}`,
+        buttons: {
+            no: {
+                text: 'Permanecer aquí',
+                value: 'stay',
+            },
+            si: {
+                text: `Ir a ${i.name}`,
+                value: 'leave',
+            },
+        },
+    })
+    .then(value => {
+        switch (value) {
+            case "stay":
+                break;
+            case "leave":
+                swal({
+                    icon: 'success',
+                    title: `En breve irás a la materia de ${i.name}`,
+                });
+                setTimeout(() => {
+                    window.open(`./pages/${i.name}.html`,'_self');
+                }, 2000);
+                break;
+        };
+    });
+    console.log(i.name)
 });
